e6a18dff6b07514f3258dfbf376b0644b83ab511
Jeroen Baartmans <jbaartmans@hotmail.com>
2021-01-02 16:25:44 +0100

    /newgame/ levert de stukken random aan.

--- src/main/java/com/baartmans/jstratego2021/GameService.java
+++ src/main/java/com/baartmans/jstratego2021/GameService.java
@@ -21,80 +21,7 @@ public class GameService{
         newGame.setTeam2(createTeam(2));
         return newGame;
     }
-
-    private LinkedList<String> getStartingPiecesInGame(){
-
-        LinkedList pieces = new LinkedList<String>();
-
-        // TODO; hoeveel stukken zijn er van elk?
-//        "Bomb"
-//        "Marshal"
-//        "General"
-//        "Colonel"
-//        "Major"
-//        "Captain"
-//        "Lieutenant"
-//        "Sergeant"
-//        "Miner"
-//        "Scout:
-//        "Spy"  1
-//        "Flag"  1
-
-
-
-//        Rang	Aantal per kleur
-//        Bom	6
-//        Maarschalk	1
-//        Generaal	1
-//        Kolonel	2
-//        Majoor	3
-//        Kapitein	4
-//        Luitenant	4
-//        Sergeant	4
-//        Mineur	5
-//        Verkenner	8
-//        Spion	1
-//        Vlag	1
-
-
-        // 6 Bombs
-        Collections.addAll(pieces, "Bomb","Bomb","Bomb","Bomb","Bomb","Bomb");
-        // 1 Maarschalk
-        pieces.add("Marshal");
-        // 1 General
-        pieces.add("General");
-        // 2 Colonels
-//        Collections.addAll(pieces, "Colonel","Colonel");
-//        // 3 Majoors
-//        Collections.addAll(pieces, "Major","Major");
-//        // 4 Kapiteins
-//        Collections.addAll(pieces, "Captain","Captain");
-//
-//        // 4 Luitenanten
-//        Collections.addAll(pieces, "Lieutenant","Captain");
-
-
-        pieces.add("Major");
-
-
-
-        Collections.addAll(pieces, "Captain","Captain","Captain","Captain","Captain");
-
-        Collections.addAll(pieces, "Sergeant","Sergeant","Sergeant","Sergeant","Sergeant");
-
-
-
-        pieces.add("Flag");
-
-
-        //Add all scoutsa
-        for(int i = 0; i < 28; i++){
-            pieces.add("Scout");
-        }
-
-        return  pieces;
-    }
-
+    
     private Pawn[] createTeam(int teamNumber){
         int yFrom = 0;
         int yTo = 3;
@@ -119,6 +46,54 @@ public class GameService{
         return team;
     }
 
+    private LinkedList<String> getStartingPiecesInGame(){
+
+        LinkedList pieces = new LinkedList<String>();
+
+//        Rang	Aantal per kleur
+//        Bom	6
+//        Maarschalk	1
+//        Generaal	1
+//        Kolonel	2
+//        Majoor	3
+//        Kapitein	4
+//        Luitenant	4
+//        Sergeant	4
+//        Mineur	5
+//        Verkenner	8
+//        Spion	1
+//        Vlag	1
+
+        // 6 Bombs
+        Collections.addAll(pieces, "Bomb","Bomb","Bomb","Bomb","Bomb","Bomb");
+        // 1 Maarschalk
+        pieces.add("Marshal");
+        // 1 General
+        pieces.add("General");
+        // 2 Colonels
+        Collections.addAll(pieces, "Colonel","Colonel");
+        // 3 Majoors
+        Collections.addAll(pieces, "Major","Major","Major");
+        // 4 Kapiteins
+        Collections.addAll(pieces, "Captain","Captain","Captain","Captain");
+
+        // 4 Luitenanten
+        Collections.addAll(pieces, "Lieutenant","Lieutenant","Lieutenant","Lieutenant");
+
+        // 4 Sergeanten
+        Collections.addAll(pieces, "Sergeant","Sergeant","Sergeant","Sergeant");
+
+        // 5 Mineurs
+        Collections.addAll(pieces, "Miner","Miner","Miner","Miner","Miner");
+
+        // 8 Verkenners
+        Collections.addAll(pieces, "Scout","Scout","Scout","Scout","Scout","Scout","Scout","Scout");
+        // 1 Spion
+        Collections.addAll(pieces, "Spy");
+        // 1 Vlag
+        pieces.add("Flag");
+        return  pieces;
+    }
 
     // TODO move this function to util class
     // Read more: https://www.java67.com/2015/01/how-to-get-random-number-between-0-and-1-java.html#ixzz6iOvlQk81
