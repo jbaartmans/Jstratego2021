461b0c5a6a2590ed141caacd263d5a3650e4d8d1
Jeroen Baartmans <jbaartmans@hotmail.com>
2021-01-04 10:41:31 +0100

    WIP: doMove implementatie.

---
+++ src/main/java/com/baartmans/jstratego2021/gamelogic/Move.java
@@ -0,0 +1,25 @@
+package com.baartmans.jstratego2021.gamelogic;
+
+public class Move {
+
+    private int[] from;
+    private int[] to;
+
+    public Move(int[] from, int[] to){
+
+        this.from = from;
+        this.to = to;
+    }
+
+    public int[] getFrom() {
+        return from;
+    }
+
+    public void setFrom(int[] from) {
+        this.from = from;
+    }
+
+    public int[] getTo() {  return to; }
+
+    public void setTo(int[] to) { this.to = to; }
+}
---
+++ src/main/java/com/baartmans/jstratego2021/gamelogic/enums/GameStatus.java
@@ -0,0 +1,5 @@
+package com.baartmans.jstratego2021.gamelogic.enums;
+
+public enum GameStatus {
+    PLAYING,WON,LOST
+}
